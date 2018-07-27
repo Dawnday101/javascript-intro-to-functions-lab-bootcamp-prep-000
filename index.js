74e729c03ced4149737f419c19b16e80a0de5873
dawnday101@gmail.com <dawnday101@gmail.com>
2018-07-27 02:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase)
 }
+function logWhisper(string) {
+  console.log(string.lowercase)
+}
+var lowercase = 'hello'
+var uppercase = 'HELLO'
+var mixedCase = 'I love you, Grandma.'
+
