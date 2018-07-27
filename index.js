6d540546721a523d7203a33810b95a372007ba9e
dawnday101@gmail.com <dawnday101@gmail.com>
2018-07-27 02:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ var lowercase = 'hello'
 var uppercase = 'HELLO'
 var mixedCase = 'I love you, Grandma.'
 
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
